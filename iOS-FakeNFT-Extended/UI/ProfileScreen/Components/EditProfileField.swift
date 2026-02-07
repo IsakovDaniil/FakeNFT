1069a520f7f6a9cfff1c230bd481c7244bdbf424
IsakovDaniil <serverpgig@yandex.ru>
2026-02-07 17:32:11 +0300

    [Feature] add component EditProfileField

--- iOS-FakeNFT-Extended/UI/ProfileScreen/Components/EditProfileField.swift
+++ iOS-FakeNFT-Extended/UI/ProfileScreen/Components/EditProfileField.swift
@@ -8,11 +8,53 @@
 import SwiftUI
 
 struct EditProfileField: View {
+    
+    let title: String
+    @Binding var text: String
+    let isMultiline: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(Font.bold22)
+                .foregroundStyle(.appBlack)
+            
+            if isMultiline {
+                TextField("", text: $text, axis: .vertical)
+                    .font(Font.regular17)
+                    .lineLimit(1...10)
+                    .padding(11)
+                    .background(.appLightGray)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+            } else {
+                TextField("", text: $text)
+                    .font(Font.regular17)
+                    .padding()
+                    .background(.appLightGray)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+            }
+        }
     }
 }
 
 #Preview {
-    EditProfileField()
+    VStack(spacing: 20) {
+        EditProfileField(
+            title: "Имя",
+            text: .constant("Joaquin Phoenix"),
+            isMultiline: false
+        )
+        
+        EditProfileField(
+            title: "Описание",
+            text: .constant("Дизайнер из Казани, люблю цифровое искусство и бейглы. В моей коллекции уже 100+ NFT, и еще больше — на моём сайте. Открыт к коллаборациям."),
+            isMultiline: true
+        )
+        
+        EditProfileField(
+            title: "Сайт",
+            text: .constant("app.ru"),
+            isMultiline: false
+        )
+    }
 }
--- iOS-FakeNFT-Extended/UI/ProfileScreen/EditProfile/EditProfileView.swift
+++ iOS-FakeNFT-Extended/UI/ProfileScreen/EditProfile/EditProfileView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct EditProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ProfileAvatar(image: Image(.placeholderAvatar))
+            EditProfileButton(name: "Сохранить") {
+                print("tap")
+            }
+        }
     }
 }
 
