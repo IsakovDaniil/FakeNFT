111c205efc67e3aacb0a2c94a36e1149096cce0d
IsakovDaniil <serverpgig@yandex.ru>
2026-02-07 14:39:44 +0300

    [Feature] add bioSection

--- iOS-FakeNFT-Extended/UI/Profile/ProfileView.swift
+++ iOS-FakeNFT-Extended/UI/Profile/ProfileView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ProfileView: View {
     
     // MARK: - Properties
+    var userName: String
+    var bio: String
+    var website: String
     
     // MARK: - Body
     
@@ -17,10 +20,13 @@ struct ProfileView: View {
         ZStack {
             Color.appWhite
                 .ignoresSafeArea()
-            VStack {
+            VStack(alignment: .leading, spacing: 20) {
                 avatarNameSection
+                
+                bioSection
             }
         }
+        .padding()
     }
     
     // MARK: - View Components
@@ -28,14 +34,26 @@ struct ProfileView: View {
         HStack(spacing: 16) {
             ProfileAvatar(image: Image(.placeholderAvatar))
             
-            Text("Joaquin Phoenix")
+            Text(userName)
                 .font(Font.bold22)
                 .foregroundStyle(.appBlack)
+        }
+    }
+    
+    private var bioSection: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(bio)
+                .font(Font.regular13)
             
+            if let url = URL(string: "https://\(website)") {
+                Link(website, destination: url)
+                    .font(Font.regular15)
+                    .foregroundStyle(.appBlue)
+            }
         }
     }
 }
 
 #Preview {
-    ProfileView()
+    ProfileView(userName: "Joaquin Phoenix", bio: "Дизайнер из Казани, люблю цифровое искусство  и бейглы. В моей коллекции уже 100+ NFT,  и еще больше — на моём сайте. Открыт к коллаборациям.", website: "JoaquinPhoenix.com")
 }
