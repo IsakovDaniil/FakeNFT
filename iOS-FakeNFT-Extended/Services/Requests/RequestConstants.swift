db6e0aacb884462baccaa72d88600ef16cb5de6a
Sultan <sultanisabayev2@gmail.com>
2026-02-07 19:55:52 +0500

    feature: добавил страницу WebViewScreen

--- iOS-FakeNFT-Extended/Services/Requests/RequestConstants.swift
+++ iOS-FakeNFT-Extended/Services/Requests/RequestConstants.swift
@@ -1,5 +1,5 @@
 enum RequestConstants {
     static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
 #warning("insert your token here")
-    static let token = ""
+    static let token = "39cd2798-f008-4902-9435-9d9f21360bd5"
 }
