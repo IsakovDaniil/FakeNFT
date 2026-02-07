518a2ac20e6683bbaf3a507c16a12c3a2bfd07a0
IsakovDaniil <serverpgig@yandex.ru>
2026-02-07 18:06:15 +0300

    [Refactor] EditProfileView

--- iOS-FakeNFT-Extended/UI/ProfileScreen/EditProfile/EditProfileView.swift
+++ iOS-FakeNFT-Extended/UI/ProfileScreen/EditProfile/EditProfileView.swift
@@ -8,35 +8,51 @@
 import SwiftUI
 
 struct EditProfileView: View {
+    
+    // MARK: - Properties
+    
+    @State private var name: String = "Joaquin Phoenix"
+    @State private var description: String = "Дизайнер из Казани, люблю цифровое искусство и бейглы. В моей коллекции уже 100+ NFT, и еще больше — на моём сайте. Открыт к коллаборациям."
+    @State private var website: String = "Joaquin Phoenix.com"
+    
+    // MARK: - Body
+    
     var body: some View {
         VStack(spacing: 20) {
-            ProfileAvatar(image: Image(.placeholderAvatar), editMode: true)
+            avatarSection
             
-            EditProfileField(
-                title: "Имя",
-                text: .constant("Joaquin Phoenix"),
-                isMultiline: false
-            )
-            
-            EditProfileField(
-                title: "Описание",
-                text: .constant("Дизайнер из Казани, люблю цифровое искусство и бейглы. В моей коллекции уже 100+ NFT, и еще больше — на моём сайте. Открыт к коллаборациям."),
-                isMultiline: true
-            )
-            
-            EditProfileField(
-                title: "Сайт",
-                text: .constant("app.ru"),
-                isMultiline: false
-            )
+            fieldsSection
             
             Spacer()
             
-            EditProfileButton(name: "Сохранить") {
-                print("tap")
-            }
+            buttonSection
         }
         .padding()
+    }
+}
+
+// MARK: - Subviews
+
+private extension EditProfileView {
+    
+    var avatarSection: some View {
+        ProfileAvatar(image: Image(.placeholderAvatar), editMode: true) {
+            print("tap")
+        }
+    }
+    
+    var fieldsSection: some View {
+        VStack(spacing: 20) {
+            EditProfileField(title: "Имя", text: $name, isMultiline: false)
+            EditProfileField(title: "Описание", text: $description, isMultiline: true)
+            EditProfileField(title: "Сайт", text: $website, isMultiline: false)
+        }
+    }
+    
+    var buttonSection: some View {
+        EditProfileButton(name: "Сохранить") {
+            print("tap button")
+        }
     }
 }
 
