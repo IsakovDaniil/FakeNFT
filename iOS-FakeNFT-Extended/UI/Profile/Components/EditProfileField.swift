c6bd55f038c136d7ae31bd611597d61638d0f92b
IsakovDaniil <serverpgig@yandex.ru>
2026-02-07 16:44:01 +0300

    [Feature] add swift files

---
+++ iOS-FakeNFT-Extended/UI/Profile/Components/EditProfileField.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileField.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Даниил on 07.02.2026.
+//
+
+import SwiftUI
+
+struct EditProfileField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileField()
+}
---
+++ iOS-FakeNFT-Extended/UI/Profile/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Даниил on 07.02.2026.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
