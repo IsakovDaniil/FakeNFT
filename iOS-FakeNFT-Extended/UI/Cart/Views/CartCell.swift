111fb4543e611f7eabc00d2c96e231b0f88dffd8
Sultan <sultanisabayev2@gmail.com>
2026-02-12 11:34:11 +0500

    feature: Реализованы все API методы во вью модели, также с NetworkRequest-ами и моделями

--- iOS-FakeNFT-Extended/UI/Cart/Views/CartCell.swift
+++ iOS-FakeNFT-Extended/UI/Cart/Views/CartCell.swift
@@ -26,7 +26,7 @@ struct CartCell: View {
                 }
 
                 VStack(alignment: .leading, spacing: 2) {
-                    Text("Цена")
+                    Text("cart.nft.price")
                         .font(.regular13)
 
                     Text("\(priceText) ETH")
