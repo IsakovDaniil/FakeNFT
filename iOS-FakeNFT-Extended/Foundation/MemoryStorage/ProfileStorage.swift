9e90aef68d828eb7078d3bc2f31ff27ff4c8865c
IsakovDaniil <serverpgig@yandex.ru>
2026-02-13 16:49:55 +0300

    [Feature] add ProfileStorage

--- iOS-FakeNFT-Extended/Foundation/MemoryStorage/ProfileStorage.swift
+++ iOS-FakeNFT-Extended/Foundation/MemoryStorage/ProfileStorage.swift
@@ -11,6 +11,46 @@ import Foundation
 
 protocol ProfileStorageProtocol: AnyObject {
     func saveProfile(_ profile: UserProfile) async
-    func getProfile() async -> UserProfile
+    func getProfile() async -> UserProfile?
     func clearProfile() async
+}
+
+actor ProfileStorage: ProfileStorageProtocol {
+    
+    private enum Keys {
+        static let profile = "cached_user_profile"
+    }
+    
+    private let userDefaults: UserDefaults
+    private let encoder: JSONEncoder
+    private let decoder: JSONDecoder
+    
+    init(
+        userDefaults: UserDefaults,
+        encoder: JSONEncoder,
+        decoder: JSONDecoder
+    ) {
+        self.userDefaults = userDefaults
+        self.encoder = encoder
+        self.decoder = decoder
+    }
+    
+    func saveProfile(_ profile: UserProfile) async {
+        guard let data = try? encoder.encode(profile) else { return }
+        
+        userDefaults.set(data, forKey: Keys.profile)
+    }
+    
+    func getProfile() async -> UserProfile? {
+        guard let data = userDefaults.data(forKey: Keys.profile) else { return nil }
+        
+        guard let profile = try? decoder.decode(UserProfile.self, from: data) else { return nil }
+        
+        return profile
+    }
+    
+    func clearProfile() async {
+        userDefaults.removeObject(forKey: Keys.profile)
+    }
+    
 }
