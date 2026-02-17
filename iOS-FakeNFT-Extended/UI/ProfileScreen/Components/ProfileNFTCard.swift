1dbe2fcfa9ec83105a889834ffe20c22814b4a60
IsakovDaniil <serverpgig@yandex.ru>
2026-02-17 18:13:12 +0300

    [Feature] add NFTCard

---
+++ iOS-FakeNFT-Extended/UI/ProfileScreen/Components/ProfileNFTCard.swift
@@ -0,0 +1,73 @@
+//
+//  ProfileNFT.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Даниил on 17.02.2026.
+//
+
+import SwiftUI
+
+struct ProfileNFTCard: View {
+    
+    // MARK: - Size
+    
+    enum NFTSize {
+        case myNFT
+        case favorites
+        
+        var cardSize: CGFloat {
+            switch self {
+            case .myNFT:     return 110
+            case .favorites: return 80
+            }
+        }
+        
+        var likePadding: CGFloat {
+            switch self {
+            case .myNFT:     return 10
+            case .favorites: return 5
+            }
+        }
+    }
+    
+    // MARK: - Properties
+    
+    let image: String
+    let size: NFTSize
+    @State var isLiked: Bool
+    
+    // MARK: - Body
+    
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            cardImage
+            likeButton
+        }
+    }
+    
+    // MARK: - Subviews
+    
+    @ViewBuilder
+    private var cardImage: some View {
+        Image(image)
+            .resizable()
+            .scaledToFill()
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .frame(width: size.cardSize, height: size.cardSize)
+    }
+    
+    private var likeButton: some View {
+        Image(.like)
+            .foregroundStyle(isLiked ? .appRed : .appWhiteUniversal)
+            .padding(size.likePadding)
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    VStack {
+        ProfileNFTCard(image: "Lilo", size: .myNFT, isLiked: false)
+        ProfileNFTCard(image: "Pixi", size: .favorites, isLiked: true)
+    }
+}
